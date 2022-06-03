57136505d729b9425119f8ec9dbff2ed7564d7af
GefersonLopes <gefersonjefreey@gmail.com>
2022-06-03 14:41:19 -0300

    Entrega s2-06

--- src/js/dataProdutos.js
+++ src/js/dataProdutos.js
@@ -1,37 +1,128 @@
+
 const produtos = [
     {
-        nome: 'Banana',
-        preco: 2,
-        secao: 'Hortifruti',
-        categoria: 'fruta',
-        img: "./src/img/banana.png"
-    },
-    {
+        id: 1,
         nome: 'Morango',
-        preco: 2,
+        preco: '7.00',
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: './src/img/morango.png'
+        img: './src/img/morango.png',
+        promocao: false,
+        precoPromocao: '0',
+        componentes: [
+            'Fibras',
+            'Vitamina C',
+            'Cálcio',
+            'Ferro'
+        ]
     },
     {
-        nome: 'Maçã',
-        preco: 2,
+        id: 2,
+        nome: 'Banana',
+        preco: '4.00',
         secao: 'Hortifruti',
         categoria: 'fruta',
-        img: './src/img/maca.png'
+        img: "./src/img/banana.png",
+        promocao: true,
+        precoPromocao: '1.00',
+        componentes: [
+            'Potássio',
+            'Vitamina B6',
+            'Vitamina C',
+            'Folatos'
+        ]
     },
     {
+        id: 3,
+        nome: 'Yogurt de Morango',
+        preco: '3.00',
+        secao: 'Laticinio',
+        categoria: 'Yogurts',
+        img: './src/img/yogurt de morango.png',
+        promocao: true,
+        precoPromocao: '1.50',
+        componentes: [
+            'Potássio',
+            'Vitamina A',
+            'Vitamina E',
+            'Vitamina C'
+        ]
+    },
+    {
+        id: 4,
         nome: 'Pão',
-        preco: 4,
+        preco: '5.00',
         secao: 'Panificadora',
         categoria: 'Pães',
-        img: './src/img/pao.png'
+        img: './src/img/pao.png',
+        promocao: true,
+        precoPromocao: '2.50',
+        componentes: [
+            'Cálcio',
+            'Sódio',
+            'Fibra Alimentar',
+            'Proteínas'
+        ]
     },
     {
+        id: 5,
         nome: 'Leite',
-        preco: 5,
-        secao: 'Laticínio',
+        preco: '7.00',
+        secao: 'Laticinio',
         categoria: 'Leite',
-        img: './src/img/leite.png'
-    }
+        img: './src/img/leite.png',
+        promocao: false,
+        precoPromocao: '1.00',
+        componentes: [
+            'Carboidratos',
+            'Proteínas',
+            'Gorduras totais'
+        ]
+    },
+    {
+        id: 6,
+        nome: 'Bolo',
+        preco: '10.00',
+        secao: 'Panificadora',
+        categoria: 'Bolos',
+        img: './src/img/bolo.png',
+        promocao: false,
+        precoPromocao: '1.00',
+        componentes: [
+            'Carboidratos',
+            'Proteínas',
+            'Gorduras totais'
+        ]
+    },
+    {
+        id: 7,
+        nome: 'Torta de Maça',
+        preco: '4.00',
+        secao: 'Hortifruti',
+        categoria: 'Leite',
+        img: './src/img/torta de maca.png',
+        promocao: false,
+        precoPromocao: '1.00',
+        componentes: [
+            'Carboidratos',
+            'Proteínas',
+            'Gorduras totais'
+        ]
+    },
+    {
+        id: 8,
+        nome: 'Maçã',
+        preco: '4.00',
+        secao: 'Hortifruti',
+        categoria: 'fruta',
+        img: './src/img/maca.png',
+        promocao: true,
+        precoPromocao: '1.50',
+        componentes: [
+            'Potássio',
+            'Vitamina A',
+            'Vitamina E',
+            'Vitamina C'
+        ]
+    },
 ]
--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,16 @@
-function soma(somar){
-    let guardarValor=0;
-    somar.filter((somando)=>guardarValor+=somando.preco)
-    let valorTotal=document.querySelector("#precoTotal")
-    valorTotal.innerText=`${guardarValor}.00`;
-}
+const divCompra=document.querySelector(".itensSelecionados");
+const ulCarrinho=document.createElement("ul");
+const sacola=document.querySelector("#sacola");
+const Remover=document.querySelector("#Remover");
+let quantidadeCarrinho=document.getElementById("quantidadeDeItens");
+let valorCarrinho=document.getElementById("valorDosItens");
 
 
+// Criando card de produtos//
+/////////////////////////////
 function criandoProdutos(itens){
     const ul=document.querySelector("ul");
     ul.innerHTML="";
-    soma(itens);
     for(let i=0;i<produtos.length;i++){
         
         const li=document.createElement("li");
@@ -17,43 +18,129 @@ function criandoProdutos(itens){
         const h3=document.createElement("h3");
         const p=document.createElement("p");
         const span=document.createElement("span");
+        const ol=document.createElement("ol");
+        const button=document.createElement("button");
+        const divButton=document.createElement("div");
 
+
+        li.classList="itens";
         img.src=itens[i].img;
         h3.innerText=itens[i].nome;
-        p.innerText=itens[i].preco;
-        span.innerText=`seção ${itens[i].secao}`;
+        p.innerText=`R$ ${itens[i].preco}`;
+        span.innerText=`${itens[i].secao}`;
+        button.innerText="Comprar";
+        divButton.classList="Comprar";
+        button.classList="botao";
 
+        h3.classList="NomeDosCards"
+
+        itens[i].componentes.forEach((element) => {
+            let li=document.createElement("li");
+            li.innerText=element
+            ol.append(li);
+            
+            
+        });
+
+        divButton.append(button);
         ul.append(li);
-        li.append(img,h3,p,span);
+        li.append(img,h3,span,ol,p,divButton);
+        
+        
+        button.addEventListener("click",()=>{
+            adicionarAoCarrinho(itens[i]);
+            soma(itens[i]);
+        });
+      
     }
 };
 criandoProdutos(produtos);
 
 
-const botaoMostrarTodos=document.querySelector(".estiloGeralBotoes--mostrarTodos");
-botaoMostrarTodos.addEventListener("click",function chamarFuncao(){
-    criandoProdutos(produtos);
-});
+// adicionando evento aos botôes//
+//////////////////////////////////
+const mostrarTodos=document.getElementById("Todos");
+const mostrarHortifrut=document.getElementById("Hortifruti");
+const mostrarPanificadora=document.getElementById("Panificadora");
+const mostrarLaticinios=document.getElementById("Laticínios");
 
+let inputPesquisarProduto=document.querySelector(".campoBuscaPorNome");
+const botaoPesquisar=document.querySelector(".estiloGeralBotoes--botaoBuscaPorNome");
 
-const botaoMostrarFrutas=document.querySelector(".estiloGeralBotoes--filtrarHortifruti");
-botaoMostrarFrutas.addEventListener("click",function botao(){
-    const hortifrut= produtos.filter(function frutas(apenasFrutas){
-        return apenasFrutas.secao=="Hortifruti"?true:false;
-    });
-    criandoProdutos(hortifrut);
-});
+mostrarTodos.addEventListener("click",()=>criandoProdutos(produtos));
 
-
-const input=document.querySelector(".campoBuscaPorNome");
-const buttonBuscar=document.querySelector(".estiloGeralBotoes--botaoBuscaPorNome");
-buttonBuscar.addEventListener("click",function(){
-    let busca=input.value;
-    let seusProdutos=produtos.filter(function buscarItem(item){
-        return item.nome==busca?true:false;
+mostrarHortifrut.addEventListener("click",()=>{
+    const Hortifruti=produtos.filter(elemento=>{
+        return elemento.secao=="Hortifruti";
     })
-    criandoProdutos(seusProdutos);
-    
+    criandoProdutos(Hortifruti);
 });
+
+mostrarPanificadora.addEventListener("click",()=>{
+    const Panificadora=produtos.filter(elemento=>{
+        return elemento.secao=="Panificadora";
+    })
+    criandoProdutos(Panificadora)
+});
+
+mostrarLaticinios.addEventListener("click",()=>{
+    const Laticinios=produtos.filter(elemento=>{
+        return elemento.secao=="Laticinio";
+    })
+    criandoProdutos(Laticinios)
+});
+
+
+botaoPesquisar.addEventListener("click",()=>{
+    let valor=inputPesquisarProduto.value;
+    const pesquisarItem=produtos.filter(elemento=>{
+        return elemento.nome.toLowerCase().includes(valor.toLowerCase())||elemento.secao.toLowerCase().includes(valor.toLowerCase())||elemento.categoria.toLowerCase().includes(valor.toLowerCase());
+    })
+    criandoProdutos(pesquisarItem)
+});
+
+
+function adicionarAoCarrinho(conteudo){
+    
+    sacola.remove();
+    Remover.innerHTML="";
+    
+    const liCarrinho=document.createElement("li");
+    const imgCarrinho=document.createElement("img");
+    const divCarrinho=document.createElement("div");
+    const h3Carrinho=document.createElement("h3");
+    const pCarrinho=document.createElement("p");
+    const spanCarrinho=document.createElement("span");
+    
+    liCarrinho.classList="liCarrinho";
+    divCarrinho.classList="infoCarrinho";
+    h3Carrinho.classList="infoCarrinho_";
+    pCarrinho.classList="infoCarrinho_";
+    pCarrinho.id="Infosecao";
+    spanCarrinho.classList="infoCarrinho_";
+    imgCarrinho.classList="imagemCarrinho";
+
+    imgCarrinho.src=conteudo.img;
+    imgCarrinho.alt="foto";
+    h3Carrinho.innerText=conteudo.nome;
+    pCarrinho.innerText=conteudo.secao;
+    spanCarrinho.innerText=`R$ ${conteudo.preco}`;
+
+    divCarrinho.append(h3Carrinho,pCarrinho,spanCarrinho);
+    liCarrinho.append(imgCarrinho,divCarrinho);
+    ulCarrinho.append(liCarrinho);
+    divCompra.append(ulCarrinho);
+
+}
+
+let quantidade=0;
+let valorTotal=0;
+
+function soma(itens){
+    quantidade+=1;
+    valorTotal+=Number(itens.preco);
+    quantidadeCarrinho.innerText=quantidade;
+    valorCarrinho.innerText=`R$ ${valorTotal},00`;
+}
 
 
